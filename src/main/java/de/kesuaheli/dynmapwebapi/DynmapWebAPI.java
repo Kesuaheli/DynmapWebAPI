25d8880a1ab15317bcbe5e1e125d6e0e96df0662
Kesuaheli <kesuaheligame@gmail.com>
2023-07-30 17:23:50 +0200

    initial commit

---
+++ src/main/java/de/kesuaheli/dynmapwebapi/DynmapWebAPI.java
@@ -0,0 +1,12 @@
+package de.kesuaheli.dynmapwebapi;
+@SuppressWarnings("unused")
+public final class DynmapWebAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
